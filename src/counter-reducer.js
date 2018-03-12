1c6ee3b6da31e0be321306cdac0b403eb9c9e111
OlenaSkibinska <olenka.notfair.skibinska@gmail.com>
2018-03-12 17:14:45 +0200

    Lecture 8 : implementing the react counter example

--- src/counter-reducer.js
+++ src/counter-reducer.js
@@ -1,9 +1,12 @@
 import expect from 'expect';
-// import {createStore} from 'redux';
+import {createStore} from 'redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 
 const counter = (state = 0, action) => {
 
-    switch (action.type){
+    switch (action.type) {
         case 'INCREMENT':
             return state + 1;
         case 'DECREMENT':
@@ -14,64 +17,58 @@ const counter = (state = 0, action) => {
 
 };
 
-const createStore = (reducer) =>{
-    let state;
-    let listeners = [];
-
-    const getState = () => state;
-
-    const dispatch = (action) =>{
-        state = reducer(state, action);
-        listeners.forEach(listener => listener()); //calling every listeners to notify about changes.
-    };
-
-    const subscribe = (listener) =>{
-        listeners.push(listener); //any time the subscribe is called, new listener is pushed into the array.
-        return () => {
-            listeners = listeners.filter(l => l !== listener) //removes listener from the array.
-        }
-    };
-    dispatch({});
-
-    return { getState, dispatch, subscribe};
-};
-
-
+const Counter = ({value, onIncrement, onDecrement}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
 const store = createStore(counter);
-const render = () =>{
-    document.body.innerText = store.getState();
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() =>
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={() =>
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />,
+        document.getElementById('root')
+    );
 };
-// console.log(store.getState());
-// store.dispatch({type: 'INCREMENT'});
-// console.log(store.getState());
 store.subscribe(render);
 render();
 
-document.addEventListener('click', () =>{
-    store.dispatch({type: 'INCREMENT'});
-});
 
-expect (
-    counter(0, { type: 'INCREMENT' })
+
+expect(
+    counter(0, {type: 'INCREMENT'})
 ).toEqual(1);
 
-expect (
-    counter(1, { type: 'INCREMENT' })
+expect(
+    counter(1, {type: 'INCREMENT'})
 ).toEqual(2);
 
-expect (
-    counter(2, { type: 'DECREMENT' })
+expect(
+    counter(2, {type: 'DECREMENT'})
 ).toEqual(1);
 
-expect (
-    counter(1, { type: 'DECREMENT' })
+expect(
+    counter(1, {type: 'DECREMENT'})
 ).toEqual(0);
 
-expect (
-    counter(1, { type: 'SOMETHING_ELSE' })
+expect(
+    counter(1, {type: 'SOMETHING_ELSE'})
 ).toEqual(1);
 
-expect (
+expect(
     counter(undefined, {})
 ).toEqual(0);
 console.log('All tests passed!');
